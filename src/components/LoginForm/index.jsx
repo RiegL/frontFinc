f8fcbc22f8dd749d75265607219cdcc6d9f306c9
RiegL <leoorieg@gmail.com>
2024-08-21 22:34:17 -0300

    criado e estilizado page login e register - cadastro de novo usuario ok - autenticação login em andamento

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,24 +1,53 @@
 "use client";
 
+import { Stack } from "@mui/material";
+import * as styles from "./style";
 import { useState } from "react";
+import axios from "axios";
 
-export const  LoginForm = () => {
-    const [message, setMessage] = useState()
+export const LoginForm = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // setMessage('Formulário enviado')
-        console.log('Formulário enviado')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // setMessage('Formulário enviado')
+    console.log("email:", email);
+    console.log("senha:", password);
+    try {
+      const response = await axios.post("http://localhost:8080/auth/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", response.data.data.token); // Salvar token no local storage
+    } catch (error) {
+      console.log("Erro ao logar", error.message);
     }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
+      <Stack maxWidth={250} gap={1}>
         <h1>login</h1>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="E-mail" required />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Password" required />
-        <button type="submit">Login</button>
+        <styles.TextField
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          label="E-mail"
+          variant="outlined"
+          required
+        />
+        <styles.TextField
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          label="Senha"
+          variant="outlined"
+          type="password"
+          required
+        />
+        <styles.Button color="success" type="submit" variant="contained">
+          Entrar
+        </styles.Button>
+      </Stack>
     </form>
   );
 };

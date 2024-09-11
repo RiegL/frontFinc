527a23c069385b1cf8f827a80b9987ef4f221f82
RiegL <leoorieg@gmail.com>
2024-09-10 22:06:15 -0300

    finalizado tabela de transações

--- src/components/Transacoes/TransacoesList/index.jsx
+++ src/components/Transacoes/TransacoesList/index.jsx
@@ -1,50 +1,71 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
 
 export const TransacoesList = () => {
+  const [transacoes, setTransacoes] = useState([]);
+
+  useEffect(() => {
+    const getTransacao = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:8080/transacoes", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        setTransacoes(response.data.data);
+      } catch (error) {
+        console.error("Erro ao buscar categorias", error.message);
+      }
+    };
+    getTransacao();
+  }, []);
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
+    <TableContainer 
+      component={Paper} 
+      sx={{ padding: 5, maxWidth: 1000, boxShadow: 3, borderRadius: 2 }}
+    >
+      <Table sx={{ minWidth: 500 }} size="small" aria-label="tabela de transações">
+        <TableHead sx={{ backgroundColor: "#f5f5f5" }}>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell sx={{ fontWeight: 'bold' }}>Descrição</TableCell>
+            <TableCell align="right" sx={{ fontWeight: 'bold' }}>Transação</TableCell>
+            <TableCell align="right" sx={{ fontWeight: 'bold' }}>Data</TableCell>
+            <TableCell align="right" sx={{ fontWeight: 'bold' }}>Situação</TableCell>
+            <TableCell align="right" sx={{ fontWeight: 'bold' }}>Valor</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {transacoes.map((transacao) => (
             <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              key={transacao.descricao}
+              sx={{
+                "&:last-child td, &:last-child th": { border: 0 },
+                backgroundColor: dayjs(transacao.data).isBefore(dayjs()) ? '#e0f7fa' : '#ffebee',
+              }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {transacao.descricao}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{transacao.tipo}</TableCell>
+              <TableCell align="right">
+                {dayjs(transacao.data).format("D MMM, YYYY")}
+              </TableCell>
+              <TableCell align="right">
+                {dayjs(transacao.data).isBefore(dayjs()) ? 'realizada' : 'planejado'}
+              </TableCell>
+              <TableCell align="right" sx={{ fontWeight: 'bold' }}>
+                R$ {(transacao.valor / 100).toFixed(2)}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -52,5 +73,3 @@ export const TransacoesList = () => {
     </TableContainer>
   );
 }
-
-export default TransacoesList;
